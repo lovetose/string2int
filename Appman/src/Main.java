80bbb1d9780d01b7e9397595080650e3aea703f1
Somkieat Wongseansuk <somkieat.wongseansuk@gmail.com>
2020-06-20 20:49:09 +0700

    Add files via upload

--- Appman/src/Main.java
+++ Appman/src/Main.java
@@ -2,25 +2,15 @@
 public class Main {
 
 	public static void main(String[] args) {
-			string2int("1asda12377313asd");
-		}
-		
-
-	
+			System.out.println(string2int("121545kjhkjhk")); 
+	}
 	public static int string2int(String input) {
-		String number = input.replaceAll("[^0-9]", "");
-		
-		String[] input2 = number.split("");
-		int[] output = new int[input2.length];
-		for(int i = 0; i < input2.length; i++) {
-			Integer j = Integer.valueOf(input2[i]);
-			output[i] = j;
-		}		
-		int toNumber=0;
-		for(int l = 0; l < output.length; l++) {
-			toNumber = 10 * toNumber + output[l];
+		String num = input.replaceAll("[^0-9]", "");
+		int output = 0;
+		for(int i = 0; i < num.length(); i++) {
+			output = output * 10 + num.charAt(i)-'0';
 		}
-		return toNumber;
+		return output;
 	}
 
 }
